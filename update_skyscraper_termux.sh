04da13023aecb7cc7c953c5f8f357f8ab6479bbe
SoumyBhow <42043870+SoumyBhow@users.noreply.github.com>
2021-08-14 18:44:00 -0400

    Add script

---
+++ update_skyscraper_termux.sh
@@ -0,0 +1,32 @@
+if [[ ! -f "$PREFIX/bin/Skyscraper" ]]; then
+echo 'First time setup'
+
+echo 'Installing required packages...'
+
+
+pkg install x11-repo -y >> /dev/null 2>&1
+pkg install git wget make clang qt5-qtbase -y >> /dev/null 2>&1
+
+echo 'Creating and moving into source folder "skysource"...'
+mkdir -p $HOME/skysource && cd $HOME/skysource >> /dev/null 2>&1
+
+echo 'Downloading source and compiling Skyscraper'
+wget -q -O - https://raw.githubusercontent.com/muldjord/skyscraper/master/update_skyscraper.sh | bash >> /dev/null 2>&1
+else
+cd $HOME/skysource
+echo 'Skyscraper binary detected. Updating and compiling...'
+./update_skyscraper.sh >> /dev/null 2>&1
+fi
+
+echo 'Manually copying the Skyscraper binary to /usr/bin...'
+
+
+cp Skyscraper $PREFIX/bin/Skyscraper
+echo 'Making the Skyscraper binary executable...'
+
+
+chmod +x $PREFIX/bin/Skyscraper
+
+echo 'Done! Please run the Skyscraper command to make sure it works'
+
+read -n 1 -r -s -p $'Press any key to continue...\n'
