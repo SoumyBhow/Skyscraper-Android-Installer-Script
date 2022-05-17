a9a64b51b0ae54377867b0aa7f7dd0574fd95a0a
SoumyBhow <soumybhow1.0@gmail.com>
2022-05-17 18:11:12 -0400

    Pass shell check

--- termux_update_skyscraper.sh
+++ termux_update_skyscraper.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 if [[ ! -f "$PATH/Skyscraper" ]]; then
 echo 'First time setup'
 echo 'Please grant Termux storage permissions in the next screen...'
@@ -11,12 +12,12 @@ pkg install x11-repo -y
 pkg install git wget ffmpeg build-essential qt5-qtbase -y
 
 echo 'Creating and moving into source folder "skysource"...'
-mkdir -p ~/skysource && cd ~/skysource
+mkdir -p ~/skysource && cd ~/skysource || exit
 
 echo 'Downloading source and compiling Skyscraper'
 wget -q -O - https://raw.githubusercontent.com/muldjord/skyscraper/master/update_skyscraper.sh | bash
 else
-cd ~/skysource
+cd ~/skysource || exit
 echo 'Skyscraper binary detected. Updating and compiling...'
 ./update_skyscraper.sh
 fi
@@ -24,11 +25,11 @@ fi
 echo 'Manually copying the Skyscraper binary to /usr/bin...'
 
 
-cp Skyscraper $PATH/Skyscraper
+cp Skyscraper "$PATH"/Skyscraper
 echo 'Making the Skyscraper binary executable...'
 
 
-chmod +x $PATH/Skyscraper
+chmod +x "$PATH"/Skyscraper
 
 echo 'Done! Please run the Skyscraper command to make sure it works'
 
