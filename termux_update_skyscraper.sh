0bf53493d0833ab8608c3cb40287fd713077ac39
SoumyBhow <soumybhow1.0@gmail.com>
2022-08-17 19:01:23 -0400

    Use curl instead of wget cause I'm stupid

--- termux_update_skyscraper.sh
+++ termux_update_skyscraper.sh
@@ -5,10 +5,10 @@ pkg install x11-repo -y
 pkg install git wget ffmpeg build-essential qt5-qtbase whiptail ninja liblz4 libuv -y
 
 mkdir -p ~/SkyscraperAndroid && cd ~/SkyscraperAndroid || exit
-wget -q -O - https://raw.githubusercontent.com/SoumyBhow/skyscraper/scripted/update_skyscraper.sh || bash
+curl -O -L -q https://raw.githubusercontent.com/SoumyBhow/skyscraper/scripted/update_skyscraper.sh || bash
 cd ~/SkyscraperAndroid || exit
-wget -q -O - https://raw.githubusercontent.com/SoumyBhow/CHDMAN-and-Maxcso-Android-Installer/scripted/Compressor.sh || bash
+curl -O -L -q https://raw.githubusercontent.com/SoumyBhow/CHDMAN-and-Maxcso-Android-Installer/scripted/Compressor.sh || bash
 cd ~/SkyscraperAndroid || exit
-wget -q -O - https://raw.githubusercontent.com/SoumyBhow/Skyscraper-Lazy-Script/master/SkyscraperLazy.sh
+curl -O -L -q https://raw.githubusercontent.com/SoumyBhow/Skyscraper-Lazy-Script/master/SkyscraperLazy.sh
 cp SkyscraperLazy.sh $PATH/Lazy && chmod +x $PATH/Lazy
 exit
