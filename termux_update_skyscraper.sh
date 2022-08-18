9cdf9bca8d7b0432ca2a4d0e6d560bf641d1eabe
SoumyBhow <soumybhow1.0@gmail.com>
2022-08-17 20:18:33 -0400

    Minor tweak

--- termux_update_skyscraper.sh
+++ termux_update_skyscraper.sh
@@ -5,9 +5,9 @@ pkg install x11-repo -y
 pkg install git wget ffmpeg build-essential qt5-qtbase whiptail ninja liblz4 libuv -y
 
 mkdir -p ~/SkyscraperAndroid && cd ~/SkyscraperAndroid || exit
-curl https://raw.githubusercontent.com/SoumyBhow/skyscraper/scripted/update_skyscraper.sh && chmod +x update_skyscraper.sh && bash update_skyscraper.sh
-curl https://raw.githubusercontent.com/SoumyBhow/CHDMAN-and-Maxcso-Android-Installer/scripted/Compressor.sh && chmod +x Compressor.sh
+curl -O https://raw.githubusercontent.com/SoumyBhow/skyscraper/scripted/update_skyscraper.sh && chmod +x update_skyscraper.sh && bash update_skyscraper.sh
+curl -O https://raw.githubusercontent.com/SoumyBhow/CHDMAN-and-Maxcso-Android-Installer/scripted/Compressor.sh && chmod +x Compressor.sh
 cd ~/SkyscraperAndroid || exit
-curl https://raw.githubusercontent.com/SoumyBhow/Skyscraper-Lazy-Script/master/SkyscraperLazy.sh
+curl -O https://raw.githubusercontent.com/SoumyBhow/Skyscraper-Lazy-Script/master/SkyscraperLazy.sh
 cp SkyscraperLazy.sh $PATH/Lazy && chmod +x $PATH/Lazy
 exit
