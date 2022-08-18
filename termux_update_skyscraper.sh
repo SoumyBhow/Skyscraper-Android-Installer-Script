a6baae27257ac9070eeb29d92fe067523fa48b38
SoumyBhow <soumybhow1.0@gmail.com>
2022-08-17 20:58:00 -0400

    Add missing dependency

--- termux_update_skyscraper.sh
+++ termux_update_skyscraper.sh
@@ -2,7 +2,7 @@
 termux-setup-storage
 pkg upgrade -y
 pkg install x11-repo -y
-pkg install git wget ffmpeg build-essential qt5-qtbase whiptail ninja liblz4 libuv -y
+pkg install git wget ffmpeg build-essential binutils qt5-qtbase whiptail ninja liblz4 libuv -y
 
 mkdir -p ~/SkyscraperAndroid && cd ~/SkyscraperAndroid || exit
 curl -O https://raw.githubusercontent.com/SoumyBhow/skyscraper/scripted/update_skyscraper.sh && chmod +x update_skyscraper.sh && bash update_skyscraper.sh
