58b13d49bc06e246cfb78c57e3009b8efd0590ce
SoumyBhow <soumybhow1.0@gmail.com>
2022-08-17 21:15:55 -0400

    Fix directory change

--- termux_update_skyscraper.sh
+++ termux_update_skyscraper.sh
@@ -3,12 +3,13 @@ termux-setup-storage
 pkg upgrade -y
 pkg install x11-repo -y
 pkg install git wget ffmpeg build-essential binutils qt5-qtbase whiptail ninja liblz4 libuv -y
-
 mkdir -p ~/SkyscraperAndroid && cd ~/SkyscraperAndroid || exit
-curl -O https://raw.githubusercontent.com/SoumyBhow/skyscraper/scripted/update_skyscraper.sh && chmod +x update_skyscraper.sh && bash update_skyscraper.sh
-cd ~/SkyscraperAndroid || exit
-curl -O https://raw.githubusercontent.com/SoumyBhow/CHDMAN-and-Maxcso-Android-Installer/scripted/Compressor.sh && chmod +x  && bash Compressor.sh
-cd ~/SkyscraperAndroid || exit
+curl -O https://raw.githubusercontent.com/SoumyBhow/skyscraper/scripted/update_skyscraper.sh
+curl -O https://raw.githubusercontent.com/SoumyBhow/CHDMAN-and-Maxcso-Android-Installer/scripted/Compressor.sh
 curl -O https://raw.githubusercontent.com/SoumyBhow/Skyscraper-Lazy-Script/master/SkyscraperLazy.sh
+bash update_skyscraper.sh
+cd ~/SkyscraperAndroid || exit
+bash Compressor.sh
+cd ~/SkyscraperAndroid || exit
 cp SkyscraperLazy.sh $PATH/Lazy && chmod +x $PATH/Lazy
 exit
