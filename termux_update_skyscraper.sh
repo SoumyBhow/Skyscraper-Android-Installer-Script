5ac9a394c988647dbb04ddfe093f0d558f1583d4
SoumyBhow <soumybhow1.0@gmail.com>
2022-08-17 21:23:54 -0400

    Minor tweak

--- termux_update_skyscraper.sh
+++ termux_update_skyscraper.sh
@@ -4,8 +4,8 @@ pkg upgrade -y
 pkg install x11-repo -y
 pkg install git wget ffmpeg build-essential binutils qt5-qtbase whiptail ninja liblz4 libuv -y
 mkdir -p ~/SkyscraperAndroid && cd ~/SkyscraperAndroid || exit
-curl -O https://raw.githubusercontent.com/SoumyBhow/skyscraper/scripted/update_skyscraper.sh
-curl -O https://raw.githubusercontent.com/SoumyBhow/CHDMAN-and-Maxcso-Android-Installer/scripted/Compressor.sh
+curl -O https://raw.githubusercontent.com/SoumyBhow/skyscraper/master/update_skyscraper.sh
+curl -O https://raw.githubusercontent.com/SoumyBhow/CHDMAN-and-Maxcso-Android-Installer/master/Compressor.sh
 curl -O https://raw.githubusercontent.com/SoumyBhow/Skyscraper-Lazy-Script/master/SkyscraperLazy.sh
 bash ~/SkyscraperAndroid/update_skyscraper.sh
 bash ~/SkyscraperAndroid/Compressor.sh
