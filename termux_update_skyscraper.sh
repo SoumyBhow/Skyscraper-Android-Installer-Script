dbe12fe4ae3984e7c978657ebd61e13e2803fc69
SoumyBhow <soumybhow1.0@gmail.com>
2022-08-17 21:18:03 -0400

    Fix actual script execution

--- termux_update_skyscraper.sh
+++ termux_update_skyscraper.sh
@@ -7,9 +7,7 @@ mkdir -p ~/SkyscraperAndroid && cd ~/SkyscraperAndroid || exit
 curl -O https://raw.githubusercontent.com/SoumyBhow/skyscraper/scripted/update_skyscraper.sh
 curl -O https://raw.githubusercontent.com/SoumyBhow/CHDMAN-and-Maxcso-Android-Installer/scripted/Compressor.sh
 curl -O https://raw.githubusercontent.com/SoumyBhow/Skyscraper-Lazy-Script/master/SkyscraperLazy.sh
-bash update_skyscraper.sh
-cd ~/SkyscraperAndroid || exit
-bash Compressor.sh
-cd ~/SkyscraperAndroid || exit
+bash ~/SkyscraperAndroid/update_skyscraper.sh
+bash ~/SkyscraperAndroid/Compressor.sh
 cp SkyscraperLazy.sh $PATH/Lazy && chmod +x $PATH/Lazy
 exit
